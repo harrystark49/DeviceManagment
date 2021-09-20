3341dc1f15b593f86f2c1243f7692566ae522141
Nikhil kumar <nikhil.singarapu@nukkadshops.com>
2021-09-20 17:44:10 +0530

    Nikhil

--- app/src/main/java/com/example/mini_proect/fragments/All_devices.kt
+++ app/src/main/java/com/example/mini_proect/fragments/All_devices.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.mini_proect.R
 
 
 class all_devices : Fragment() {
+    var Devices: ArrayList<dataclass> = arrayListOf()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +25,30 @@ class all_devices : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all_devices, container, false)
+        var view = inflater.inflate(R.layout.fragment_all_devices, container, false)
+
+        initRecycler()
+        val recycler = view.findViewById<RecyclerView>(R.id.recyclerView)
+        var recycle: RecyclerView = recycler
+        var LLM: LinearLayoutManager = LinearLayoutManager(context)
+        LLM.orientation = RecyclerView.VERTICAL
+        recycle.layoutManager = LLM
+        var adapter = Adapter(Devices)
+        recycle.adapter = adapter
+
+
+
+
+        return view
+    }
+
+
+    private fun initRecycler() {
+        Devices.add(dataclass("Trychup Herbal", "hcvjd", "hjfqe", "gqfqjfq"))
+        Devices.add(dataclass("Trychup Herbal", "hcvjd", "hjfqe", "gqfqjfq"))
+        Devices.add(dataclass("Trychup Herbal", "hcvjd", "hjfqe", "gqfqjfq"))
+        Devices.add(dataclass("Trychup Herbal", "hcvjd", "hjfqe", "gqfqjfq"))
+
     }
 
 
---
+++ app/src/main/java/com/example/mini_proect/fragments/dataclass.kt
@@ -0,0 +1,3 @@
+package com.example.mini_proect.fragments
+
+data class dataclass(val device_Id:String,val phonetype:String,val Manufacture:String,val Version:String )
