7df312150244cf5c31c42cd6d8de00a4f8c3da1a
Pudindi Durga Prasad <durgaprasad.pudindi@xcubelabs.com>
2021-09-20 15:19:57 +0530

    Fragments

--- app/src/main/java/com/example/mini_proect/tests/Admin_settings.kt
+++ app/src/main/java/com/example/mini_proect/tests/Admin_settings.kt
@@ -1,27 +1,40 @@
-package com.example.mini_proect.tests
+package Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+
 import com.example.mini_proect.R
 
 
-class Admin_settings : Fragment() {
 
+class admin_Settings : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_settings, container, false)
-    }
+        // Inflate the layout for this fragments
+        val view = inflater.inflate(R.layout.fragment_admin_settings, container, false)
 
+
+//        view.update_profile.setOnClickListener {
+//            val i = Intent(context,RegisterActivity::class.java)
+//            startActivity(i)
+//        }
+//        view.change_password.setOnClickListener {
+//            val i = Intent(context,ChangePassword::class.java)
+//            startActivity(i)
+//        }
+        return view
+
+    }
 }
