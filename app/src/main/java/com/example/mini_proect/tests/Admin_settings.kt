184eb3c25cebcc9767c55548ff022959b4026349
Pudindi Durga Prasad <durgaprasad.pudindi@xcubelabs.com>
2021-09-20 15:29:36 +0530

    Fragments

--- app/src/main/java/com/example/mini_proect/tests/Admin_settings.kt
+++ app/src/main/java/com/example/mini_proect/tests/Admin_settings.kt
@@ -1,40 +1,56 @@
-package Fragments
+package com.example.mini_proect.tests
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-
 import com.example.mini_proect.R
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
-
-
-class admin_Settings : Fragment() {
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Admin_settings.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Admin_settings : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragments
-        val view = inflater.inflate(R.layout.fragment_admin_settings, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_admin_settings, container, false)
+    }
 
-
-//        view.update_profile.setOnClickListener {
-//            val i = Intent(context,RegisterActivity::class.java)
-//            startActivity(i)
-//        }
-//        view.change_password.setOnClickListener {
-//            val i = Intent(context,ChangePassword::class.java)
-//            startActivity(i)
-//        }
-        return view
-
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment Admin_settings.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic fun newInstance(param1: String, param2: String) =
+                Admin_settings().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
+                }
     }
 }
