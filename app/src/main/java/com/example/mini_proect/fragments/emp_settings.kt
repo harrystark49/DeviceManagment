7aebfa4e7721bcdf805c9a355bc32bba61e129f9
Pudindi Durga Prasad <durgaprasad.pudindi@xcubelabs.com>
2021-09-22 17:07:57 +0530

    emp settings

--- app/src/main/java/com/example/mini_proect/fragments/emp_settings.kt
+++ app/src/main/java/com/example/mini_proect/fragments/emp_settings.kt
@@ -6,9 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.mini_proect.Activities.AdminDetails
 import com.example.mini_proect.Activities.ChangePassword
+import com.example.mini_proect.Activities.Register
 import com.example.mini_proect.R
 import kotlinx.android.synthetic.main.fragment_admin_settings.view.*
+import kotlinx.android.synthetic.main.fragment_emp_settings.view.*
 
 
 class emp_settings : Fragment() {
@@ -28,17 +31,22 @@ class emp_settings : Fragment() {
         val email = b!!.getString("EmpEmail").toString()
         val pass = b!!.getString("EmpPass").toString()
 
-        view.update_profile.setOnClickListener {
-
+        view.emp_update_profile.setOnClickListener {
+                    val intent=Intent(context,Register::class.java)
+            startActivity(intent)
         }
-        view.change_password.setOnClickListener {
+        view.emp_change_password.setOnClickListener {
 
             val intent = Intent(context, ChangePassword::class.java)
             intent.putExtra("EmpEmail",email)
             intent.putExtra("EmpPass",pass)
-
             startActivity(intent)
         }
+        view.adminDetails.setOnClickListener {
+            val intent = Intent(context,AdminDetails::class.java)
+            startActivity(intent)
+        }
+
         return view
     }
 
